a79dccab1690374fc109920fb7bd0c1b72b575c6
Nelson Brandão <nelsonbrandao800@gmail.com>
2023-10-06 18:37:38 +0100

    Add menubar app skeleton

---
+++ menubar/src/App.tsx
@@ -0,0 +1,8 @@
+export const App = () => {
+  // async function greet() {
+  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  //   setGreetMsg(await invoke('greet', { name }));
+  // }
+
+  return <div className="flex"></div>;
+};
